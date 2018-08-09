47f04760e34a1e1fc3fd82abb50e43877579382a
Igor Yalovoy <igor@yalovoi.net>
2018-08-09 21:01:05 +0300

    init

---
+++ contracts/CoinPledge.sol
@@ -0,0 +1,122 @@
+// Author: Igor Yalovoy
+// Web: ylv.io
+// Email: to@ylv.io
+// GitHub: https://github.com/ylv-io/coinpledge/tree/master
+// Twitter: https://twitter.com/ylv_io
+
+// Coin Pledge
+// Archive your goals and have fun with friends. Powered by security of smart contracts.
+
+
+// Proofs:
+// Public commitment as a motivator for weight loss (https://onlinelibrary.wiley.com/doi/pdf/10.1002/mar.20316)
+
+
+pragma solidity ^0.4.17;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/payment/PullPayment.sol";
+import "openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
+
+contract CoinPledge is Ownable, CanReclaimToken, PullPayment {
+
+  uint constant daysToJudge = 7 days;
+  uint constant bonusPercentage = 50;
+  uint constant feePercentage = 1;
+
+  struct Challenge {
+    string name;
+    uint value;
+    address judge;
+    uint startDate;
+    uint time;
+
+    bool successed;
+    bool resolved;
+  }
+
+  event NewChallenge(uint challengeId, string name, uint value, address judge, uint startDate, uint time);
+  event ChallengeResolved(uint challengeId, bool decision);
+
+  Challenge[] public challenges;
+
+  mapping(uint => address) public challengeToOwner;
+  mapping(address => uint) public ownerToChallengeCount;
+  mapping(address => uint) public bonusFund;
+
+  function getChallenges(address owner) external view returns(uint[]) {
+    uint[] memory result = new uint[](ownerToChallengeCount[owner]);
+    uint counter = 0;
+    for (uint i = 0; i < challenges.length; i++) {
+      if (challengeToOwner[i] == owner)
+      {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+  }
+
+
+  function createChallenge(string name, address judge, uint time) external payable returns (uint retId)
+  {
+    require(msg.value >= 0.01 ether, "Has to stake more than 0.01 ether");
+
+    uint startDate = block.timestamp;
+    uint id = challenges.push(Challenge(name, msg.value, judge, startDate, time, false, false)) - 1;
+    challengeToOwner[id] = msg.sender;
+
+    emit NewChallenge(id, name, msg.value, judge, startDate, time);
+
+    return id;
+  }
+
+  function resolveChallenge(uint challengeId, bool decision) external
+  {
+    Challenge storage challenge = challenges[challengeId];
+    address challenger = challengeToOwner[challengeId];
+
+    require(challenge.resolved == false, "Challenge already resolved.");
+    require(block.timestamp > (challenge.startDate + challenge.time), "It is not time yet to judge.");
+
+    // if more time passed than endDate + daysToJudge, then challenger can resolve himself
+    if(block.timestamp > (challenge.startDate + challenge.time + daysToJudge))
+      require(challenge.judge == msg.sender || challenger == msg.sender, "You are not the judge for this challenge.");
+    else
+      require(challenge.judge == msg.sender, "You are not the judge for this challenge.");
+
+    // write decision
+    challenge.successed = decision;
+    challenge.resolved = true;
+
+    // pay 1% fee
+    uint fee = SafeMath.mul(SafeMath.div(challenge.value, 100), feePercentage);
+    asyncTransfer(owner, fee);
+
+    uint remainingValue = SafeMath.sub(challenge.value, fee);
+
+    uint valueToPay;
+
+    if(decision) {
+      // value to pay back to challenger
+      valueToPay = remainingValue;
+      // credit bouns if any
+      if(bonusFund[challenger] > 0)
+      {
+        uint bonusValue = SafeMath.mul(SafeMath.div(bonusFund[challenger], 100), bonusPercentage);
+        bonusFund[challenger] -= bonusValue;
+      }
+    }
+    else 
+        // if failed to archive goal, put money to bonus fund
+        bonusFund[challenger] += remainingValue;
+
+    // pay back to the challenger
+    if(valueToPay > 0)
+        challenger.transfer(valueToPay);
+
+    emit ChallengeResolved(challengeId, decision);
+  }
+
+}
---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Web3 from 'web3'
+import './../css/index.css'
+
+class App extends React.Component {
+   constructor(props){
+      super(props)
+      this.state = {
+      }
+
+      if(typeof web3 != 'undefined') {
+         console.log("Using web3 detected from external source like Metamask")
+         this.web3 = new Web3(web3.currentProvider)
+      } else {
+         console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+         this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+      }
+
+      const MyContract = web3.eth.contract([{"constant":false,"inputs":[],"name":"generateNumberWinner","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"myid","type":"bytes32"},{"name":"result","type":"string"}],"name":"__callback","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numberOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_queryId","type":"bytes32"},{"name":"_result","type":"string"},{"name":"_proof","type":"bytes"}],"name":"__callback","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"player","type":"address"}],"name":"checkPlayerExists","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"resetData","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"bets","type":"uint256"}],"name":"updateMaxBets","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"number","type":"uint256"}],"name":"bet","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"amountWei","type":"uint256"}],"name":"updateMinimumBet","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"distributePrizes","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numberWinner","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"minimumBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"maxAmountOfBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalBet","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_maxAmountOfBets","type":"uint256"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"}])
+      this.state.ContractInstance = MyContract.at("0x430d959fa54714aca8eecd61fae2661fca900e04")
+
+      window.a = this.state
+   }
+
+   componentDidMount() {
+      this.updateState()
+      this.setupListeners()
+
+      setInterval(this.updateState.bind(this), 7e3)
+   }
+
+   updateState() {
+   }
+
+   // Listen for events
+   setupListeners() {
+   }
+
+   render(){
+      return (
+          <div>
+            <h1>Hello</h1>
+          </div>
+      )
+   }
+}
+
+ReactDOM.render(
+   <App />,
+   document.querySelector('#root')
+)
